d43f8b2cfcb64055ce2c61215eceae8cd1b88e5a
rusty619 <rusty6916@gmail.com>
2022-02-07 17:12:58 -0500

    commit tree 46ed8a6a61d7923f1af27223f25e0266d988cce2

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string){
+    //todo
+    return string.toUpperCase()
+}
+
+function whisper(string){
+    //todo
+    return string.toLowerCase();
+}
+
+function logShout(string){
+    console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string){
+    if(string === whisper(string)){
+        return "I can't hear you!"
+    } else if(string === shout(string)) return "YES INDEED!";
+      else if (string === "Let's have dinner together!") return "I would love to!";
+}
